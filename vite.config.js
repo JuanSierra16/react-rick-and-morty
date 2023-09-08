c6cef737958ac2d3f3863737e9d99d60b70a543a
Juan Jose <juanjose.sierra@utp.edu.co>
2023-09-08 18:46:23 -0500

    react-rick-and-morty

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://JuanSierra16.github.io/react-rick-and-morty/",
   plugins: [react()],
+  base: "https://JuanSierra16.github.io/react-rick-and-morty/",
 })
